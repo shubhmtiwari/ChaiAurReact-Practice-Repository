a459c1e19fd9628ab0e7a9e18c4fcad606bdb7d4
Shubham <tiwshubham36@gmail.com>
2026-05-09 01:47:39 +0530

    Learned about js and jsx files in both the projects

---
+++ basicreact01/src/App.js
@@ -0,0 +1,13 @@
+import Chai from "./chai";
+
+function App() {
+  // return <h1>Chai aur react | Shubham Tiwari</h1>;
+
+  return (
+    <>
+      <Chai />
+    </>
+  );
+}
+
+export default App;
---
+++ vitereact01/src/App.jsx
@@ -0,0 +1,33 @@
+import Chai from "./chai.jsx";
+
+function App() {
+  return (
+    // <div>
+    //   <h1>Chai Aur React| Shubham Tiwari</h1>
+    //   <li>
+    //     1. Learnt how to use react with framework and without framework using
+    //     bundlers like vite
+    //   </li>
+    //   <li>
+    //     2. React Core Library and the attachment we get with library like
+    //     react-dom and react-native etc.
+    //   </li>
+    //   <li>
+    //     3. Two ways to create react projects - first using npx create-react-app
+    //     command and another using vite
+    //   </li>
+    //   <li>4. Learnt why we prefer using vite over create-react-app</li>
+    //   <li>
+    //     5. How to understand the files inside our react project, understood the
+    //     package.json file, and different scripts
+    //   </li>
+    //   <li>
+    //     6. Most importantly learnt to clean up the code after creating the react
+    //     project.
+    //   </li>
+    // </div>
+    <Chai />
+  );
+}
+
+export default App;
